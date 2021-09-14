2f413a923b171d8240539654d2ff92df3761fcec
Brandon <nickelboarder123.bl@gmail.com>
2021-09-13 22:33:50 -0400

    comment

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,7 @@
 package org.example;
 
 /*
- *  UCF COP3330 Fall 2021 Assignment 6 Solution
+ *  UCF COP3330 Fall 2021 Assignment 9 Solution
  *  Copyright 2021 Brandon Lopez
  */
 import java.util.Scanner;
